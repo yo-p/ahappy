458221c3b3bb49df33bc60f9bf0e7cdeb889581e
yo-p <papapapipipi42@gmail.com>
2020-03-22 04:52:34 +0000

    add comment

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,7 +1,11 @@
 class Tweet < ApplicationRecord
     attachment :image #tweetに画像も投稿
+
     belongs_to :user 
-    belongs_to :genre, optional: true #開発途中ジャンル空でも投稿可能 
+    
+    belongs_to :genre, optional: true #開発途中ジャンル空でも投稿可能
+    
+    has_many :comments, dependent: :destroy
 
     validates :title, presence: true
     validates :body, presence: true
