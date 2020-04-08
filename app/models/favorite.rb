081235b2806e2f92d526cb1b4d6a0fede3bbe374
yo-p <papapapipipi42@gmail.com>
2020-04-08 12:11:00 +0000

    erorrs ja

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -4,7 +4,8 @@ class Favorite < ApplicationRecord
   belongs_to :user
   belongs_to :tweet
 
-
+  default_scope -> { order(created_at: :desc) }
+  
   # モデルレベルで一意性（ユニーク）を保つ
   validates :user_id, uniqueness: { scope: :tweet_id }
 end
--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,6 @@ module Ahappy
     # the framework and any gems in your application.
     config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
