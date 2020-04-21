90f1738d228701d44de752887a57f8d7521c2b72
yo-p <papapapipipi42@gmail.com>
2020-04-21 08:25:50 +0000

    modified views

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -1,14 +1,14 @@
 class NoticesController < ApplicationController
 
-    def index
-        @notices = current_user.passive_notices
-        @notices.where(checked: false).each do |notice|
-            notice.update_attributes(checked: true)
-        end
+  def index
+    @notices = current_user.passive_notices
+    @notices.where(checked: false).each do |notice|
+      notice.update_attributes(checked: true)
     end
+  end
 
-    def destroy
-        @notices = current_user.passive_notices.destroy_all
-        redirect_to notices_path
-    end
+  def destroy
+    @notices = current_user.passive_notices.destroy_all
+    redirect_to notices_path
+  end
 end
